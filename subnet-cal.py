fe5ed40fb348f3c51e45d58c16bb04b80d6cd668
RamHack <35464886+RamHack@users.noreply.github.com>
2018-01-31 10:41:06 -0400

    Update subnet-cal.py

--- subnet-cal.py
+++ subnet-cal.py
@@ -1,4 +1,4 @@
-class Subnet_cal:
+class Subnet_Cal:
     def __init__(self, ip_add, binary):  # function to solving ip addrees in binary
         self.binary = one-zero
         ip = ip.split(".")
@@ -6,6 +6,16 @@ class Subnet_cal:
         address = [adding[0:8], adding[8:16], adding[16:24], adding[24:32]]
         
         self.show_address = []
+        for a in range(len(ip_add)):
+            b = ip_add[c]
+            c = (address[c], 2)
+            self.show_address.append(b + c)
         
         
-        
+     def subnet(self, count):
+        one-zero = self.prefix 
+        adding = "1"*one-zero + "0"*(32-one-zero)
+        address = [adding[0:8], adding[8:16], adding[16:24], adding[24:32]]
+        move = math.ceil( one-zero / 8.0 )
+        host = (move*8) - one-zero
+       
