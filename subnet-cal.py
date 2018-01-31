c0d0cf3bf3fbc9a066e8fa0112867c80f8fa30a3
RamHack <35464886+RamHack@users.noreply.github.com>
2018-01-31 10:18:17 -0400

    Create subnet-cal.py

---
+++ subnet-cal.py
@@ -0,0 +1,11 @@
+class Subnet_cal:
+    def __init__(self, ip_add, binary):  # function to solving ip addrees in binary
+        self.binary = one-zero
+        ip = ip.split(".")
+        adding = "1"*one-zero + "0"*(32-one-zero)
+        address = [adding[0:8], adding[8:16], adding[16:24], adding[24:32]]
+        
+        self.show_address = []
+        
+        
+        
